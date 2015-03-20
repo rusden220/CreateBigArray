bcb8ab3a31420bbecf5d238ec064239c8a235e6c
rusden220 <surin.denis.k@gmail.com>
2015-03-20 15:28:15 +0300

    "Create a array size of 4 gb in C#"

---
+++ CreateBigArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CreateBigArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Create a big array");
+#if x64
+			int len = int.MaxValue / 2;
+#else
+			int len = int.MaxValue / 8;
+#endif
+			float[] array = new float[len];
+
+			Parallel.For(0, len, (i) =>{
+				array[i] = i;
+			});
+			Console.WriteLine("size of array: " + ((long)sizeof(float) * len) );
+			//marshal is dropping
+			//Console.WriteLine("size of array: " + System.Runtime.InteropServices.Marshal.SizeOf(array));
+			Console.ReadLine();
+		}
+	}
+}
